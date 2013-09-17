d2f08d4fe47b2047bb9c7aee62214ee03de0769b
Joshua Olson <jolson@machinezone.com>
2013-09-16 18:46:21 -0700

    version 0.0.2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,27 @@
 # rulers/lib/rulers.rb
 require "rulers/version"
+require "rulers/routing"
 require "rulers/array"
 
 module Rulers
   class Application
     def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},
-       ["Hello from Ruby on Rulers!"]]
+       [text]]
+    end
+  end
+
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
