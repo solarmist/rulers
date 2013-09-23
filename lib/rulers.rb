e2e12063b0d7b1a5fea38c5762c54ac64ae52229
Joshua Olson <joshua.olson@gmail.com>
2013-09-22 20:49:55 -0700

    Add some exception handling and redirect for the index

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -6,11 +6,23 @@ require "rulers/array"
 module Rulers
   class Application
     def call(env)
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
-      text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'},
-       [text]]
+      if env['PATH_INFO'] == '/'
+        return [302, {'Location' => 'http://localhost:3001/quotes/a_quote'}, []]
+      end
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+      begin
+        klass, act = get_controller_and_action(env)
+        controller = klass.new(env)
+        text = controller.send(act)
+        return [200, {'Content-Type' => 'text/html'},
+         [text]]
+      rescue
+        return [500, {'Content-Type' => 'text/html'},
+         ["Something went wrong on the page" +
+         "\n<pre>\n#{env}\n</pre>"]]
+      end
     end
   end
 
