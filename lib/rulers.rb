08d1a8687f6916906229be252b11ba0be5545b9b
Joshua Olson <joshua.olson@onlive.com>
2013-10-21 16:10:59 -0700

    Chapter 6

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -12,17 +12,24 @@ module Rulers
   class Application
     def call(env)
       if env['PATH_INFO'] == '/'
-        return [302, {'Location' => 'http://localhost:3001/quotes/a_quote'}, []]
-      end
-      if env['PATH_INFO'] == '/favicon.ico'
+        return [302, {'Location' => 'http://localhost:3001/quotes/index'}, []]
+      elsif env['PATH_INFO'] == '/quotes/'
+        return [302, {'Location' => 'http://localhost:3001/quotes/index'}, []]
+      elsif env['PATH_INFO'] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
       end
+
       begin
         klass, act = get_controller_and_action(env)
         controller = klass.new(env)
         text = controller.send(act)
-        return [200, {'Content-Type' => 'text/html'},
-         [text]]
+        if controller.get_response
+          st, hd, rs = controller.get_response.to_a
+          [st, hd, [rs.body].flatten]
+        else
+          render act, :obj => text
+          # [200, {'Content-Type' => 'text/html'}, [text]]
+        end
       rescue StandardError
         return [500, {'Content-Type' => 'text/html'},
                 ["Something went wrong on the page",
--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -1,11 +1,35 @@
 # rulers/lib/rulers/controller.rb
 
 require "erubis"
+require "rack/request"
+
 require "rulers/file_model"
 
 module Rulers
   class Controller
     include Rulers::Model
+
+    def response(text, status = 200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response  # Only for Rulers
+      @response
+    end
+
+    def render(*args)
+      response(render_view(*args))
+    end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params
+    end
 
     def initialize(env)
       @env = env
@@ -21,7 +45,7 @@ module Rulers
       Rulers.to_underscore klass
     end
 
-    def render(view_name, locals = {})
+    def render_view(view_name, locals = {})
       filename = File.join "app", "views",
         controller_name, "#{view_name}.html.erb"
       template = File.read filename
